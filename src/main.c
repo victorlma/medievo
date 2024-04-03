f9ba9579ed40b2a137c2e5b2de5cf6f14ccf3bdb
Victorlma <victorlima1414x@gmail.com>
2024-04-03 12:08:10 -0300

    Drawing Game Abstraction

--- src/main.c
+++ src/main.c
@@ -1,20 +1,32 @@
 #include "locdef.h"    // Localization Strings for simple translation support
 #include "raylib.h"
 
+#include "gameobjects.h"
+
+void DrawEntities()
+{
+}
+
+void GameUpdateRender()
+{
+        BeginDrawing();
 
 
+        DrawEntities();
 
+
+        EndDrawing();
+}
 
 int main()
 {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 
-    InitWindow(800, 600, "Test");
+    InitWindow(800, 600, locGAME_TITLE);
 
     while (!WindowShouldClose()) {
 
-        BeginDrawing();
-        EndDrawing();
+        GameUpdateRender();
 
     }
 
